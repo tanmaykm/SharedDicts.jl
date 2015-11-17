fb90aac88ddf7d22a062a7f1e0143eae0131eff3
tan <tanmaykm@gmail.com>
2015-11-17 12:09:57 +0530

    support bits types only

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SharedDicts
 
 [![Build Status](https://travis-ci.org/tanmaykm/SharedDicts.jl.svg?branch=master)](https://travis-ci.org/tanmaykm/SharedDicts.jl)
+
+A shared memory dictionary implemented using SharedArray and Dict.
+
+Limited functionality as of now:
+- read only
+- keys and values must be bits type
--- src/SharedDicts.jl
+++ src/SharedDicts.jl
@@ -1,5 +1,13 @@
 module SharedDicts
 
-# package code goes here
+using Base: KeyIterator, ValueIterator
+
+import Base: getindex, haskey, in, start, done, next, isempty, length
+if isdefined(Base, :share)
+    import Base.share
+end
+export SharedDict, share, getindex, haskey, in, start, done, next, isempty, length
+
+include("shareddict.jl")
 
 end # module
